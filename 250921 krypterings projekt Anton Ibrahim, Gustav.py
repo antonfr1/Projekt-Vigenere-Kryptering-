342151365a3a305c13a26c354c53dd4553b53d4f
antonfr1 <antonfridell08@gmail.com>
2025-09-21 00:42:36 +0200

    Update 250921 krypterings projekt Anton Ibrahim, Gustav.py
    samme som uden noter

--- 250921 krypterings projekt Anton Ibrahim, Gustav.py
+++ 250921 krypterings projekt Anton Ibrahim, Gustav.py
@@ -1,6 +1,6 @@
 # Modulo værdi (brug af %). tæl op indtils du når et tal og så start forfra.
 
-def lavKryptedList(string): # Etablere en funktion der laver en sting om til en liste med ascii værdier.
+def lavAsciiList(string): # Etablere en funktion der laver en sting om til en liste med ascii værdier.
     liste = [] # laver en tom liste.
     for i in string: # for hvert bogstav i strengen .
         liste.append(ord(i)) # tilføj ascii værdien af bogstavet til listen.
@@ -15,7 +15,7 @@ def kryptering(kryptKey,tekst): # funktion der krypterer teksten.
         kryptedTekstString += chr(kryptedTekst) # tilsætter det krypterede bogstav til den krypterede tekst string vi lavede i starten af funktionen.
     
     print (kryptedTekstString) # viser den krypterede tekst til brugeren.
-    return lavKryptedList(kryptedTekstString) # returner den krypterede tekst som en liste med ascii værdier.
+    return lavAsciiList(kryptedTekstString) # returner den krypterede tekst som en liste med ascii værdier.
 
 def deKryption(kryptKeyAscii,kryptedTekst): # funktion der dekrypterer teksten.
     deKryptedTekstString = "" # laver en tom string til den dekrypterede tekst.
@@ -30,15 +30,16 @@ def deKryption(kryptKeyAscii,kryptedTekst): # funktion der dekrypterer teksten.
 kryptOrDekrypt = input("Vil du kryptere eller dekryptere? (k/d): ") # spørger brugeren om de vil kryptere eller dekryptere.
 kryptDone = False # laver en variabel der holder styr på om der lige er blevet krypteret.
 if  kryptOrDekrypt == "k": # hvis brugeren vil kryptere.
-    tekstList = lavKryptedList(input("hvad vil du kryptere: ")) # spørger brugeren hvad de vil kryptere og laver det om til en liste med ascii værdier.
-    kryptKey = lavKryptedList(input("din personlige krypt key: ")) # spørger brugeren om deres krypterings nøgle og laver det om til en liste med ascii værdier.
+    tekstList = lavAsciiList(input("hvad vil du kryptere: ")) # spørger brugeren hvad de vil kryptere og laver det om til en liste med ascii værdier.
+    kryptKey = lavAsciiList(input("din personlige krypt key: ")) # spørger brugeren om deres krypterings nøgle og laver det om til en liste med ascii værdier.
     kryptedString = kryptering(kryptKey, tekstList) # kalder krypterings funktionen med krypterings nøglen og teksten brugeren vil kryptere.
     kryptDone = True # sætter variablen til sand fordi der lige er blevet krypteret.
 
 if kryptOrDekrypt == "d" or kryptDone == True: # hvis brugeren vil dekryptere eller der lige er blevet krypteret.
     if kryptDone == False: # hvis der ikke lige er blevet krypteret.
-        kryptedString = lavKryptedList(input("Hvad er den krypterede tekst? ")) # spørger brugeren hvad den krypterede tekst er og laver det om til en liste med ascii værdier.
+        kryptedString = lavAsciiList(input("Hvad er den krypterede tekst? ")) # spørger brugeren hvad den krypterede tekst er og laver det om til en liste med ascii værdier.
     opLåsNøgle = lavKryptedList(input("Hvad er nøglen? ")) # spørger brugeren om deres krypterings nøgle og laver det om til en liste med ascii værdier.
     deKryption(opLåsNøgle, kryptedString) # kalder dekrypterings funktionen med krypterings nøglen og den krypterede tekst.
 else:
+
     print("Fejl, prøv igen") # hvis brugeren ikke skrev k eller d.
